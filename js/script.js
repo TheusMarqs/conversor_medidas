b2193317c36a62dc7c9ed5393c5cd33a4c7c133c
TheusMarqs <matheusmarqsilva@gmail.com>
2023-03-22 16:09:33 -0300

    final

--- js/script.js
+++ js/script.js
@@ -1,88 +1,48 @@
-/*function Calcular() {
-    var selectInicio = document.getElementById('selectInicio').value;
-    var selectFinal = document.getElementById('selectFinal').value;
-    var valor = document.getElementById('valor').value;
-    var inputResultado = document.getElementById('inputResultado');
-
-    if (selectInicio === 'ml') {
-        if (selectFinal === 'ml'){
-            valor = valor;
-        }
-
-        else if (selectFinal === 'km') {
-            valor /= 1000;
-        }
-
-        else {
-            valor *= 3.281;
-        }
-    }
-
-    else if (selectInicio === 'km') {
-        if (selectFinal === 'ml') {
-            valor *= 1000;
-        }
-
-        else if (selectFinal === 'km'){
-            valor = valor;
-        }
-
-        else {
-            valor *= 1381;
-        }
-    }
-
-    else if (selectInicio === 'pes') {
-        if (selectFinal === 'ml') {
-            valor /= 1.381;
-        }
-
-        else if (selectFinal === 'km'){
-            valor *= 1381;
-        }
-
-        else {
-            valor = valor;
-        }
-    }
-    inputResultado.value = valor;
-}*/
-
 var v1;
 var v2;
-var valor = parseFloat(document.getElementById('valor').value);
 
 function medidaUm(m1) {
+    var btn1 = document.getElementById('btn1');
+    
     switch (m1) {
         case 'ml':
             v1 = 'ml';
+            btn1.textContent = 'Metros';
             break;
-    
+
         case 'km':
             v1 = 'km';
+            btn1.textContent = 'Quilômetros';
             break;
         case 'pes':
             v1 = 'pes';
+            btn1.textContent = 'Pés';
     }
 }
 
 function medidaDois(m2) {
+    var btn2 = document.getElementById('btn2');
     switch (m2) {
         case 'ml':
             v2 = 'ml';
+            btn2.textContent = 'Metros';
             break;
-    
+
         case 'km':
             v2 = 'km';
+            btn2.textContent = 'Quilômetros';
             break;
         case 'pes':
             v2 = 'pes';
+            btn2.textContent = 'Pés';
     }
 }
 
 function Calcular() {
+
+    var valor = parseFloat(document.getElementById('valor').value);
     if (v1 === 'ml') {
-        if (v2 === 'ml'){
+        if (v2 === 'ml') {
             valor = valor;
         }
 
@@ -100,7 +60,7 @@ function Calcular() {
             valor *= 1000;
         }
 
-        else if (v2 === 'km'){
+        else if (v2 === 'km') {
             valor = valor;
         }
 
@@ -114,7 +74,7 @@ function Calcular() {
             valor /= 1.381;
         }
 
-        else if (v2 === 'km'){
+        else if (v2 === 'km') {
             valor *= 1381;
         }
 
